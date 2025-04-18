86e8720ee430f2c1bc458f598178df6cb063de90
Woncheol Jang <sakuratsuki0111@gmail.com>
2025-04-19 00:46:57 +0900

    2025.04.19 quest

---
+++ baekjoon 1003.py
@@ -0,0 +1,8 @@
+import sys
+T = int(sys.stdin.readline())
+for _ in range(T):
+    N = int(sys.stdin.readline())
+    a, b = 1, 0
+    for i in range(N):
+        a,b = b, a+b
+    print(a,b)
---
+++ baekjoon 2579.py
@@ -0,0 +1,12 @@
+import sys
+n=int(sys.stdin.readline())
+s=[int(sys.stdin.readline()) for _ in range(n)]
+dp=[0]*(n)
+if len(s)<=2:
+    print(sum(s))
+else:
+    dp[0]=s[0]
+    dp[1]=s[0]+s[1]
+    for i in range(2,n):
+        dp[i]=max(dp[i-3]+s[i-1]+s[i], dp[i-2]+s[i])
+    print(dp[-1])
