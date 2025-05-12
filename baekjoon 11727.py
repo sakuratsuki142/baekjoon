aec0f9e5bccfd6ea83016661053a218912c0c4be
Woncheol Jang <sakuratsuki0111@gmail.com>
2025-05-12 20:24:29 +0900

    2025.05.12 quest

---
+++ baekjoon 11727.py
@@ -0,0 +1,11 @@
+import sys
+n = int(sys.stdin.readline())
+rectangular = [0] * 1001
+rectangular[1] = 1
+
+if n >= 2:
+    rectangular[2] = 3
+    for i in range(3, n+1):
+        rectangular[i] = (rectangular[i-2]*2) + rectangular[i-1]
+
+print(rectangular[n]%10_007)
