6353e0d7f81826ecb41764c687c3d1f490f5e60e
Woncheol Jang <sakuratsuki0111@gmail.com>
2025-05-21 14:30:40 +0900

    2025.05.21 quest

---
+++ baekjoon 11724.py
@@ -0,0 +1,35 @@
+import sys
+from collections import deque
+
+def bfs(start):
+    queue = deque([start])
+    visited[start] = True
+    while queue:
+        node = queue.popleft()
+        for i in graph[node]:
+            if not visited[i]:
+                visited[i] = True
+                queue.append(i)
+
+
+N, M = map(int, sys.stdin.readline().split())
+graph = [[] for _ in range(N + 1)]
+
+for _ in range(M):
+    a, b = map(int, sys.stdin.readline().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+visited = [False] * (1 + N)
+count = 0
+
+for i in range(1, N + 1):
+    if not visited[i]:
+        if not graph[i]:
+            count += 1
+            visited[i] = True
+        else:
+            bfs(i)
+            count += 1
+
+print(count)
